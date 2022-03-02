4ad0b80b5612510075116bafe54c23d7be217335
Andrea <calderato.andrea@gmail.com>
2022-03-02 07:18:37 +0100

    Aggiunto mouse look al personaggio

--- Paurissima Sprint/Assets/Paurissima Sprint/Script/MouseLook.cs
+++ Paurissima Sprint/Assets/Paurissima Sprint/Script/MouseLook.cs
@@ -9,7 +9,7 @@ public class MouseLook : MonoBehaviour
 
 	public Transform playerBody;
 
-	float xRotation = 0f;
+	float xRotation = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +19,8 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Horizontal") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Vertical") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 	
 	xRotation -= mouseY;	
 	xRotation = Mathf.Clamp(xRotation, -90f, 90f);
